21c94f1ab65f15a0ef7b0ed7331e4a7962e49278
Julian <juszko.zzx@gmail.com>
2025-10-24 16:38:13 +0200

    Added maven to the project

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,9 @@
-import javax.net.ssl.HttpsURLConnection;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
